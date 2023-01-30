041e7bd8d9c5e2b729a9167b3f7d3841b9931a46
tomer-c <tomer.c@quali.com>
2023-01-30 12:13:30 +0200

    new TF version

--- terraform/azure-vm/main.tf
+++ terraform/azure-vm/main.tf
@@ -3,17 +3,17 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "~>2.0"
+      version = "~>3.0"
     }
   }
 }
 
 provider "azurerm" {
-  features {}
-#  tenant_id       = var.tenant_id
-#  subscription_id = var.subscription_id
-#  client_id = var.client_id
-#  client_secret = var.client_secret
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
 }
 
 ## <https://www.terraform.io/docs/providers/azurerm/r/resource_group.html>
